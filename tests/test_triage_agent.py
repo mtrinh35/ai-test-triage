35c3aae3e380c9a0e2bb27515912bccee9c1556a
Matthew <matthewtrinh1517@gmail.com>
2026-09-03 22:06:29 -0700

    fixed triage import path in tests

--- tests/test_triage_agent.py
+++ tests/test_triage_agent.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import json
 from types import SimpleNamespace
 from triage_agent import triage_failure
